46c217657d914b8d642bb3532205e5e9c60119b4
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2018-06-11 14:33:56 +0900

    続きから

---
+++ ch02/初めてのニューラルネットワーク.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 13:59:04 2018
+
+@author: Takanori
+"""
+
+from keras.datasets import mnist
+(train_images, train_labes), (test_images, test_labels) = mnist.load_data()
+
+
+
+
+
+
+
+
+
+
+
+
+
